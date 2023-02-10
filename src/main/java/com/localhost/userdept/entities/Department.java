84dd43b444b1fddcf1985ea8e31e8529020aaa3f
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2023-02-10 10:39:55 -0300

    Implemented object relational mapping of entities

--- src/main/java/com/localhost/userdept/entities/Department.java
+++ src/main/java/com/localhost/userdept/entities/Department.java
@@ -1,6 +1,17 @@
 package com.localhost.userdept.entities;
 
+import javax.persistence.*;
+
+@Entity // The @Entity annotation is used to indicate that a class is also an entity.
+@Table(name = "tb_department") // The @Table annotation allows you to specify the details of the table that will
+// be used to persist the entity in the database.
 public class Department {
+
+    @Id //The I@Id annotation is used to inform JPA which field/attribute of an entity will be related
+    // to the primary key of the respective table in the database.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //The @GeneratedValue annotation is used to inform
+    // that the entity's unique identifier value generation will be managed by the persistence provider.
+    // This annotation should be added right after the @Id annotation.
     private Long id;
     private String name;
 
--- src/main/java/com/localhost/userdept/entities/User.java
+++ src/main/java/com/localhost/userdept/entities/User.java
@@ -1,10 +1,23 @@
 package com.localhost.userdept.entities;
 
+import javax.persistence.*;
+
+@Entity // The @Entity annotation is used to indicate that a class is also an entity.
+@Table(name = "tb_user") // The @Table annotation allows you to specify the details of the table that will
+// be used to persist the entity in the database.
 public class User {
+
+    @Id //The I@Id annotation is used to inform JPA which field/attribute of an entity will be related
+    // to the primary key of the respective table in the database.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // The @GeneratedValue annotation is used to inform
+    // that the entity's unique identifier value generation will be managed by the persistence provider.
+    // This annotation should be added right after the @Id annotation.
     private Long id;
     private String name;
     private String email;
 
+    @ManyToOne
+    @JoinColumn(name = "department_id")
     private Department department; // entity association
 
     //Default constructor
