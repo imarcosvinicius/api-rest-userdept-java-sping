34df992147ae0f71b678e6fbffb6b64dbc012cb1
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2023-02-10 10:53:11 -0300

    H2 database configuration

---
+++ target/classes/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_department(name) VALUES ('Tax');
+INSERT INTO tb_department(name) VALUES ('Administrative');
+
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Samantha', 'samantha@business.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Bob', 'bob@business.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Josh', 'josh@business.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Ana', 'ana@business.com');
