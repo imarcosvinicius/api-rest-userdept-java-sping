2e7f8f54a4cb10080a6906d01c43397e8c5bdbe8
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2023-02-11 09:43:10 -0300

    implementado obter usuário por id e criar novo usuário

--- src/main/java/com/localhost/userdept/controllers/UserController.java
+++ src/main/java/com/localhost/userdept/controllers/UserController.java
@@ -3,9 +3,7 @@ package com.localhost.userdept.controllers;
 import com.localhost.userdept.entities.User;
 import com.localhost.userdept.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,10 +14,21 @@ public class UserController {
     @Autowired
     private UserRepository repository;
 
-    @GetMapping
+    @GetMapping // list all users
     public List<User> findAll() {
         List<User> result = repository.findAll();
         return result;
     }
 
+    @GetMapping(value = "/{id}")// List a user by id
+    public User findById(@PathVariable Long id) {
+        User result = repository.findById(id).get();
+        return result;
+    }
+
+    @PostMapping // Create a new contact
+    public User insert(@RequestBody User user) {
+        User result = repository.save(user);
+        return result;
+    }
 }
